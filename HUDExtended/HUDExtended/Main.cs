6ac936518c62be7febc4a781de549c80117d74cf
nmar1249 <37256515+nmar1249@users.noreply.github.com>
2020-03-18 16:00:58 -0500

    - basic functionality for direction display finished

--- HUDExtended/HUDExtended/Main.cs
+++ HUDExtended/HUDExtended/Main.cs
@@ -17,8 +17,10 @@ namespace HUDExtended
     {
         public Main()
         {
+            Interval = 10;
             Tick += OnTick;
             KeyDown += OnKeyDown;
+            Notification.Show(this.Name + "has launched.");
         }
 
         //event runs every tick
@@ -30,42 +32,65 @@ namespace HUDExtended
 
 
             //show details of targeted NPC (if aiming)
-            if(Game.Player.IsTargetingAnything == true && Game.Player.IsAiming == true)
-            {
-                TargetNPC().Draw();
-            }
-            
+            TargetNPC().Draw();
+
             //show speed if in vehicle
         }
 
         //event triggered when any key is pressed
         void OnKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.J)
-            {
-                // set wanted level
-                Game.Player.WantedLevel = 0;
-                Notification.Show("Wanted Level Reset!");
-            }
+
         }
 
         TextElement GetDirection()
         {
-            PointF coords = new PointF() { X = 500, Y = 500 };
+            PointF coords = new PointF() { X = 25, Y = 500 };
+            string direction = "";
 
             var pos = Game.Player.Character.Position;
             var forward = Game.Player.Character.ForwardVector;
-            var dir = pos + forward;
+            var dir = forward;
 
-            return new TextElement(dir.ToString(), coords , .25f);
+            //set to direction vector by normalizing and rounding 
+            dir.Normalize(); 
+            dir = dir.Round(0);
+
+            // x = east, west. y = north, south.
+            // -1 means east, 1 means west
+            // -1 means south, 1 means north
+            //determine which direction the vector is pointing at 
+
+            //get Y axis first
+            if(dir.Y == 1)
+            {
+                direction += "N";
+            }
+            else if(dir.Y == -1)
+            {
+                direction += "S";
+            }
+
+            //then x axis
+            if(dir.X == 1)
+            {
+                direction += "W";
+            }
+            else if(dir.X == -1)
+            {
+                direction += "E";
+            }
+
+            return new TextElement("DIR: " + direction, coords , .5f);
         }
 
         TextElement TargetNPC()
         {
-            PointF coords = new PointF() { X = 250, Y = 250 };
+            PointF coords = new PointF() { X = 50, Y = 600 };
             Entity npc = Game.Player.TargetedEntity;
 
-            return new TextElement(npc.ToString(), coords, .25f);
+            Notification.Show("targetNPC reached DEBUG");
+            return new TextElement(npc.Handle.ToString(), coords, .5f);
         }
     }
 }
